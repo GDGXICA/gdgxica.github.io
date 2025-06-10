e0def53633d4ab9a3b19ca978420ab640e686679
michidev <michidev.code@gmail.com>
2025-06-10 18:00:15 -0500

    chore: add config to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-nocheck
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://GDGXICA.github.io",
+  base: "/gdgica.com",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
